b13c3b5b7ffbcaf8ad01ffafade6d6c1e003b45e
HEO NAMGYU <46082941+Zvckaya@users.noreply.github.com>
2025-11-21 20:17:35 +0900

    a

--- select_server/PacketTypes.h
+++ select_server/PacketTypes.h
@@ -1,7 +1,8 @@
 #pragma once
 #include <cstdint>
 
-#pragma pack(push, 1)
+#pragma pack(push, 1) //padding이 발생하지 않게 선언
+//사실 모두 4바이트라 패딩이 발생하지 않지만 일단 패킷 구조체를 선언할때는 하는게 좋다.
 
 enum class PacketType : int32_t
 {
